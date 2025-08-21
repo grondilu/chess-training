9785adcc2cdb2abde0f900cf5468c08837a44201
Lucien Grondin <grondilu@outlook.fr>
2025-08-21 13:03:54 +0200

    just before Line refactor

--- openings.js
+++ openings.js
@@ -1,6 +1,5 @@
 import { Chessground     } from './dist/chessground/src/chessground.js';
 import { Chess, SQUARES  } from './dist/chess.js/dist/esm/chess.js';
-//import { repertoires     } from './repertoires.js';
 import { book            } from './opening-book.js';
 import { SRS             } from './spaced-repetition.js';
 import { zobrist_hash    } from './zobrist.js';
@@ -8,6 +7,72 @@ import { zobrist_hash    } from './zobrist.js';
 if (typeof localStorage == 'undefined') {
 	alert("web storage is not supported");
 	throw "no web storage";
+}
+
+const white = Symbol('White');
+const black = Symbol('Black');
+function swapColor(color) { return color == white ? black : white }
+function getColor(c) { 
+    switch (c) {
+	case white: return white; break;
+	case black: return black; break;
+	default: return /^[wW](hite)?$/.test(c) ? white : black;
+    }
+}
+
+const OPENING_ASSUMPTIONS = {
+    "rnbqk2r/pppp1ppp/4pn2/8/1bPP4/2N5/PP2PPPP/R1BQKBNR w KQkq - 2 4": "Nimzo-Indian Defense",
+    "rnbqkbnr/pp2pppp/2p5/3pP3/3P4/8/PPP2PPP/RNBQKBNR b KQkq - 0 3": "Caro Kann Defense — Advanced Variation"
+}
+
+const STANDARD_MOVE_ORDERS = {
+    "Nimzo-Indian Defense": "1.d4 Nf6 2.c4 e6 3.Nc3 Bb4",
+    "Caro Kann Defense — Advanced Variation": "1.e4 c6 2.d4 d5 3.e5"
+}
+
+function extractLines(game, color) {
+
+    let result = [],
+	header;
+    let move_number = 0,
+	first_turn  = white;
+    if ("headers" in game) {
+	header = Object.fromEntries(game.headers.map($ => [$.name, $.value]));
+	if ("FEN" in header) {
+	    if (header.FEN.match(/ w /)) first_turn = black;
+	    move_number = +header.FEN.replace(/.* /, '');
+	}
+    }
+
+    (function _extractLines(moves, prefix = []) {
+	let localPrefix = [...prefix],
+	    turn = localPrefix.length % 2 == 1 ? black : white;
+	for (const move of moves) {
+	    if ("ravs" in move)
+		if (color && color !== turn) {
+		    for (let rav of move.ravs) {
+			_extractLines(rav.moves, [...localPrefix]);
+		    }
+		} else {
+		    if ("nags" in move)
+			if (['$2', '$6'].some(x => move.nags.includes(x))) {
+			    console.log(`move ${move.move} is a bad one.  Hopefully a better one is in the variations$`);
+			    if (move.ravs.length > 1) { throw "dealing with multiple variations for the repertoire color is NYI"; }
+			    else {
+				console.log(move);
+				console.warn("switching to alternate line");
+				_extractLines(move.ravs[0].moves, [...localPrefix]);
+				return result;
+			    }
+			}
+		}
+	    localPrefix.push(move);
+	    turn = swapColor(turn);
+	}
+	result.push(localPrefix);
+    })(game.moves);
+
+    return result;
 }
 
 const DEBUG = false;
@@ -41,98 +106,122 @@ const toDests = chess => {
 const AUTOMOVE_DELAY = 500; //ms
 const PENALITY_DELAY = 5000; //ms
 
-const DEFAULT_REPERTOIRES = {
-	white: "sesenar",
-	black: "tholtia"
-}
-
 // https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
 function makeMove(board, move) {
-	board.move(move.from, move.to);
-	if (move.isEnPassant()) {
-		let row = board.orientation == 'white' ? 4 : 5,
-			col = move.to.substr(0, 1);
-		board.setPieces(new Map([[col+row], [false]]));
-	}
+    board.move(move.from, move.to);
+    if (move.isEnPassant()) {
+	let row = board.orientation == 'white' ? 4 : 5,
+	    col = move.to.substr(0, 1);
+	board.setPieces(new Map([[col+row], [false]]));
+    }
 }
 
 class Line {
-	constructor(repertoire, color, moves) {
-		this.repertoire = repertoire;
-		this.color      = color;
-		this.moves = (typeof moves === 'string') ? moves.match(halfMoveRegex) : moves;
-		if (this.moves.length == 0)
-			throw `could not build line from ${moves}`;
-	}
-	pack() {
-		return [
-			this.repertoire,
-			this.color,
-			this.moves.join('')
-		].join('/')
-	}
-	pgn() {
-		let chess = new Chess();
-		for (let move of this.moves) chess.move(move);
-		return chess.pgn();
-	}
+    constructor(moves, color, start = undefined) {
+	this.moves = (typeof moves === 'string') ? moves.match(halfMoveRegex) : moves;
+	this.start = start;
+	this.color = color;
+
+	if (this.moves.length == 0)
+	    throw `could not build line from ${moves}`;
+    }
+    pack() {
+	return [
+	    this.start,
+	    this.color === white ? 'w' : 'b',
+	    this.moves.join('')
+	].join('§')
+    }
+    pgn() {
+	let chess = new Chess(),
+	    moves = [];
+	if (this.start)
+	    for (let move of STANDARD_MOVE_ORDERS[OPENING_ASSUMPTIONS[this.start]].match(halfMoveRegex))
+		chess.move(move);
+	for (let move of this.moves) chess.move(move);
+	return chess.pgn();
+    }
 }
 
 function unpack(line) {
 	if (typeof line === 'string') {
-		let     split      = line.split('/'),
-			repertoire = split[0],
-			color      = split[1],
+		let     split      = line.split('§'),
+			start      = split[0],
+			color      = split[1] === 'w' ? white : black,
 			moves      = split[2];
-		return new Line(repertoire, color, moves);
+		return new Line(moves, color, start == '' ? undefined : start);
 	} else { throw "unexpected argument type" }
 }
 
-class Repertoire {
-	constructor(name, color, lines) {
-		this.name = name;
-		this.color = color;
-		this.lines = lines;
+var lines = [];
+
+async function main() {
+
+    console.log(`loading ${lines.length} lines`);
+    console.log(lines);
+    let srs     = new SRS(storage, lines.map(x => x.pack()));
+
+    while (true) {
+	for (let div of ['info', 'pgn']) {
+	    log(`clearing ${div}`);
+	    document.getElementById(div).innerHTML = '';
 	}
+	setTimeout(() => { log("clearing stats"); document.getElementById("stats").innerHTML = '' }, 5000);
+
+	let     pick;
+	try { pick    = srs.pick(); }
+	catch(err) {
+	    document.getElementById('info').innerHTML = "error: " + err;
+	    log(err);
+	    break;
+	}
+
+	let line    = unpack(pick),
+	    success = await quiz(line);
+
+	if (success) { srs.pass(line.pack()); }
+	else         { srs.fail(line.pack()); }
+
+	document.getElementById("stats").innerHTML = Array.from(srs.stats.values()).join('/');
+    }
 }
 
-async function main(lines) {
-	let srs = new SRS(storage, lines.map(x => x.pack()));
-
-	while (true) {
-		for (let div of ['info', 'pgn']) {
-			log(`clearing ${div}`);
-			document.getElementById(div).innerHTML = '';
-		}
-		setTimeout(() => { log("clearing stats"); document.getElementById("stats").innerHTML = '' }, 5000);
-
-		let     pick    = srs.pick(),
-			line    = unpack(pick),
-			success = await quiz(line);
-
-		if (success) { srs.pass(line.pack()); }
-		else         { srs.fail(line.pack()); }
-
-		document.getElementById("stats").innerHTML = Array.from(srs.stats.values()).join('/');
-	}
-}
-
+// load repertoire from disk into memory
 Promise.all(
-	Array.from(
-		function *() {
-			for (let color of ['white', 'black']) {
-				let name = DEFAULT_REPERTOIRES[color];
-				yield fetch(`./repertoires/${color}/${name}.txt`)
-					.then(x => x.text())
-					.then(text => text.split("\n").filter(x => x))
-					.then(compacted_lines => compacted_lines.map(l => new Line(name, color, l)))
-					.then(lines => new Repertoire(name, color, lines));
+    function *() {
+	for (
+	    let pgn of [
+		"./repertoires/white/Caro-Kann/Shaw/Jouez 1.e4!/I.pgn",
+		"./repertoires/black/Nimzo-Indian/Swiercz/I/chapter-1.pgn",
+	    ]
+	) {
+	    let color = pgn.match(/\/white\//) ? white : black;
+		yield fetch(pgn)
+		.then(file => file.text())
+		.then(
+		    function (text) {
+			let games = pgnParser.parse(text, { startRule: "games" });
+			for (let game of games) {
+			    let header          = Object.fromEntries(game.headers.map($ => [$.name, $.value])),
+				extracted_lines = extractLines(game, color);
+			    for (let moves of extracted_lines) {
+				let start = header.FEN;
+				lines.push(
+				    new Line(
+					moves.map($ => $.move.replace(/[?!]/g, '')),
+					color,
+					header.FEN
+				    )
+				);
+			    };
 			}
-		}()
-	)
-).then(x => x[0].lines.concat(x[1].lines)).then(main)
+		    }
+		)
+	}
+}()
+).then(main);
 
 function identifyOpening(compacted_moves) {
 	if (compacted_moves == '') return "starting position"; 
@@ -148,88 +237,109 @@ function identifyOpening(compacted_moves) {
 	*/
 
 async function quiz(line) {
-	let chess = new Chess(),
-		color = line.color,
-		moves = [...line.moves], // for cloning
-		board = Chessground(document.getElementById('chessboard'), {});
+    console.log(line);
+    let chess = new Chess(),
+	color = line.color,
+	orientation  = getColor(color) === white ? 'white' : 'black',
+	moves = [...line.moves], // for cloning
+	board = Chessground(document.getElementById('chessboard'), {});
 
-	if (color == 'black') {
-		// make first move
-		if (moves.length > 0) {
-			let move = chess.move(moves.shift());
-			//board.move(move.from, move.to);
-			makeMove(board, move);
-			//document.getElementById('soundMove').play();
-		}
+    if (line.start) {
+	chess.load(line.start);
+	board.set({ fen: line.start });
+    }
+
+    log([line, color, orientation, getColor(chess.turn())]);
+    if (getColor(color) !== getColor(chess.turn())) {
+	// make first move
+	if (moves.length > 0) {
+	    let move = chess.move(moves.shift());
+	    //board.move(move.from, move.to);
+	    makeMove(board, move);
+	    //document.getElementById('soundMove').play();
 	}
+    } else {
+	log({ color: getColor(color), turn: chess.turn() });
+	log({ fen: line.start, newturn: getColor(chess.turn()) });
+    }
 
-	return new Promise(
-		(resolve, reject) => {
-			board.set(
-				{
-					orientation: color,
-					selectable: { enable: true },
-					events: {
-						change: () => {
-							document.getElementById("info").innerHTML = identifyOpening(chess.history().join(''));
-							document.getElementById("pgn" ).innerHTML = chess.pgn();
-						},
-					},
-					movable: {
-						free: false,
-						dests: toDests(chess),
-						showDests: true,
-						events: {
-							after: async (from, to, metadata) => {
-								document.getElementById('soundMove').play();
-								if (moves.length > 0) {
-									try {
-										let move = chess.move({ from, to }),
-											expected_move = moves.shift();
-										makeMove(board, move);
-										// Is the user's move the expected one?
-										if (move.san == expected_move) {
-											log("good move");
-											//   - shift moves
-											//   - make next half-move if defined
-											if (moves.length > 0) {
-												let nextmove = chess.move(moves.shift());
-												//board.move(nextmove.from, nextmove.to);
-												makeMove(board, nextmove);
-												board.set({ movable: { dests: toDests(chess) } });
-												document.getElementById('soundMove').play();
-											}
-											if (moves.length == 0) {
-												document.getElementById('soundSuccess').play();
-												log("success! locking board");
-												board.stop();
-												resolve(true);
-											}
-
-										} else {
-											document.getElementById('soundWrong').play();
-											log(`wrong move: ${expected_move} was expected`);
-											chess.undo();
-											board.set({ fen: chess.fen() });
-											let move = chess.move(expected_move);
-											//board.move(move.from, move.to);
-											makeMove(board, move);
-											board.stop();
-											await sleep(PENALITY_DELAY);
-											resolve(false);
-											//alert(`wrong move! ${expected_move} was expected.`);
-										}
-									}
-									catch (err) {
-										board.set({ fen: chess.fen() });
-										console.warn(err);
-									}
-								} else { console.warn("unexpected user input, stopping board"); board.stop(); }
-							}
-						}
+    return new Promise(
+	(resolve, reject) => {
+	    board.set(
+		{
+		    orientation,
+		    selectable: { enable: true },
+		    events: {
+			change: () => {
+			    let pgn = '';
+			    if (line.start)
+				document.getElementById("pgn").innerHTML = pgn = STANDARD_MOVE_ORDERS[OPENING_ASSUMPTIONS[line.start]];
+			    pgn += chess.pgn().replaceAll(/\[.*\] ?/g, '');
+			    log({chess, pgn, moves: STANDARD_MOVE_ORDERS[OPENING_ASSUMPTIONS[line.start]], start: line.start });
+			    if (pgn !== '')
+				document.getElementById("info").innerHTML = identifyOpening(pgn.match(halfMoveRegex).join(''));
+			    document.getElementById("pgn").innerHTML = pgn.replaceAll(/\[.*\] ?/g, '');
+			},
+		    },
+		    movable: {
+			free: false,
+			dests: toDests(chess),
+			showDests: true,
+			events: {
+			    after: async (from, to, metadata) => {
+				document.getElementById('soundMove').play();
+				if (moves.length > 0) {
+				    try {
+					let move = chess.move({ from, to }),
+					    expected_move = moves.shift();
+					log(["chosen move is :", move.san]);
+					makeMove(board, move);
+					// Is the user's move the expected one?
+					if (move.san == expected_move) {
+					    log("good move");
+					    //   - shift moves
+					    //   - make next half-move if defined
+					    if (moves.length > 0) {
+						let nextmove = chess.move(moves.shift());
+						//board.move(nextmove.from, nextmove.to);
+						makeMove(board, nextmove);
+						board.set({ movable: { dests: toDests(chess) } });
+						document.getElementById('soundMove').play();
+					    }
+					    if (moves.length == 0) {
+						document.getElementById('soundSuccess').play();
+						log("success! locking board");
+						board.stop();
+						resolve(true);
+					    }
 					}
+					else {
+					    document.getElementById('soundWrong').play();
+					    log(`wrong move: ${expected_move} was expected`);
+					    chess.undo();
+					    board.set({ fen: chess.fen() });
+					    let move = chess.move(expected_move);
+					    //board.move(move.from, move.to);
+					    makeMove(board, move);
+					    board.stop();
+					    log("sleeping 1s");
+					    await sleep(PENALITY_DELAY)
+					    log("resolving to false");
+					    resolve(false);
+					}
+				    }
+				    catch (err) {
+					board.set({ fen: chess.fen() });
+					console.warn(["caught error", err]);
+				    }
 				}
-			);
-		});
+				else { console.warn("unexpected user input, stopping board"); board.stop(); }
+			    }
+			}
+		    }
+		}
+	    );
+	});
 }
 
+// vi: shiftwidth=4
